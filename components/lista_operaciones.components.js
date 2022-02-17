dcd173da1d140b72e2f2c51f87d053b8795e06a1
Lgutarra <99109411+Lgutarra@users.noreply.github.com>
2022-02-17 02:11:28 -0500

    Create lista_operaciones.components.js

---
+++ components/lista_operaciones.components.js
@@ -0,0 +1,35 @@
+const ListaOperaciones = (props) => {
+    return <main className="col-md-10">
+        <h1>Lista de operaciones</h1>
+        <table className="table">
+            <thead class="table-dark">
+                <tr>
+                    <th>Id</th>
+                    <th>Fecha y hora</th>
+                    <th>Cliente</th>
+                    <th>Tipo de operación</th>
+                    <th>Tipo de cambio</th>
+                    <th>Monto</th>
+                    <th>Editar</th>
+                </tr>
+            </thead>
+            <tbody id="data_operaciones">
+                {
+                    props.operaciones.map((operacion) => {
+                        return <tr key={operacion.nombre}>
+                            <td>{operacion.id}</td>
+                            <td>{operacion.fechahora}</td>
+                            <td>{operacion.cliente}</td>
+                            <td>{operacion.tipo}</td>
+                            <td>{operacion.cambio}</td>
+                            <td>{operacion.monto}</td>
+                            <td><a href="../historia34">Editar</a></td>
+                        </tr>
+                    })
+                }
+            </tbody>
+        </table>
+    </main>
+}
+
+export default ListaOperaciones
