d1df7cdafcbbace84adf1228783c079414234913
20181390@aloe.ulima.edu.pe <MaritzaPalomino>
2022-02-16 19:22:30 -0500

    Page Iniciar Sesion

---
+++ pages/iniciarSesionCliente.js
@@ -0,0 +1,13 @@
+import FormularioLogin from "../components/formulario_login.components"
+
+export default function IniciarSesion(){
+    return <div> 
+    <div id="general">
+        <h1 id="titulo">Iniciar Sesion</h1>
+        <div id="formulario">
+        <FormularioLogin />
+        </div>
+    </div>
+    </div>
+
+}
