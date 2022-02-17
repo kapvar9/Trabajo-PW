eb6cc6fd1906beb95ae5913578c746ce35f1f50d
Lgutarra <99109411+Lgutarra@users.noreply.github.com>
2022-02-17 02:16:12 -0500

    Create finalizar_boton.components.js

---
+++ components/finalizar_boton.components.js
@@ -0,0 +1,4 @@
+const FinalizarBoton = (props) =>{
+    return <button id="butFinalizar" onClick={props.finalizar} className="btn btn-primary float-right" type="button">Finalize</button>
+}
+export default FinalizarBoton
