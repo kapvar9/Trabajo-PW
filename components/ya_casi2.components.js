6dcc83c07b1aba5551a2cfef44446e90c3aca1a0
Lgutarra <99109411+Lgutarra@users.noreply.github.com>
2022-02-24 19:14:34 -0500

    Create ya_casi2.components.js

---
+++ components/ya_casi2.components.js
@@ -0,0 +1,34 @@
+const YaCasi2 = () => {
+
+    return <aside class="col-md-8">
+        <div class="card ">
+            <div class="card-body" >
+                <div className="container text-center">
+                    <form className="form-label-group">
+                        <div className="col-md-12">
+                            <label for="txt_username" class="form label"><b>¡Ya casi estamos listos!</b></label>
+                        </div>
+                        <div className="col-md-15">
+                            <label for="txt_username" class="form label">Nuestro número de cuenta BCP es &nbsp;XXX-XXXXX-XXXXXXXXXX</label>
+                        </div>
+
+                        <div className="row mb-1">
+                            <div className="col-md-7">
+                                <label for="txt_username" class="form label">Número de transacción</label>
+                            </div>
+                            <div className="col-md-1">
+                                <input type="text" id="disabledTextInput" />
+                            </div>
+                        </div>
+                        <div className="col-md-15">
+
+                            <a href="../historia31b">Siguiente </a>
+                        </div>
+                    </form>
+                </div>
+
+            </div>
+        </div>
+    </aside>
+}
+export default YaCasi2 
