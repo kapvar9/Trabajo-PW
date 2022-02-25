c0516e89b9555898e75b404692c69072524d32ea
Acissej9 <cotrina.ejci@gmail.com>
2022-02-24 23:36:48 -0500

    commint numbilletera

---
+++ components/nbilletera.components.js
@@ -0,0 +1,30 @@
+const nbilletera = () => {
+
+    return <aside class="col-md-8">
+        <div class="card ">
+            <div class="card-body" >
+                <div className="container text-center">
+                    <form className="form-label-group">
+                        <div className="col-md-12">
+                            <label for="txt_username" class="form label"><b>¡Ingresa tu número de billetera !</b></label>
+                        </div>
+                        
+                        <div className="row mb-3">
+                           
+                            <div className="col-md-1">
+                                <input type="text" id="disabledTextInput" />
+                            </div>
+                        </div>
+                        <div className="col-md-15">
+
+                            <a href="../historia30b">Siguiente </a>
+                        </div>
+                    </form>
+                </div>
+
+            </div>
+        </div>
+    </aside>
+}
+export default nbilletera
+
