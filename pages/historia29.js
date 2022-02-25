4bcfbbd4a42a8e9a7a404a8b65935e936e28d2b9
Lgutarra <99109411+Lgutarra@users.noreply.github.com>
2022-02-24 19:11:00 -0500

    Create historia29.js

---
+++ pages/historia29.js
@@ -0,0 +1,46 @@
+import Footer from "../components/footer.components"
+import MenuNavegacionCliente from "../components/menu_navegacion_cliente.components"
+import CerrarSesionBoton from "../components/cerrarsesion_boton.components"
+import DireccionBilletera from "../components/direccionbilletera.components"
+import { Image } from "react-bootstrap"
+
+function Historia29() {
+    const CerrarHandler = () => {
+        location.href = "/"
+    }
+    return <div>
+        <div className="bd-header bg-dark py-3 d-flex align-items-stretch border-bottom border-dark">
+            {/**Agregar cabecera */}
+            <header className="container-fluid d-flex align-items-center">
+                <div className="d-flex align-items-center fs-4 text-white mb-0">
+                <Image src="/Images/LogoBitcoin.png" className="logo me-3" width="46" height="38" alt="Bootstrap"/>
+                    <h1>Crypto-nita</h1>
+                </div>
+                <div className="ms-auto">
+                    {/**Se llama al componente botones */}
+                    <CerrarSesionBoton cerrar={CerrarHandler} />
+                </div>
+
+            </header>
+        </div>
+        <div>
+            <MenuNavegacionCliente />
+        </div>
+        <div>
+            <span id="texto 1" className="ejemplo1">
+                <font face="Cascadia Code" color="black" size="40">Operación de compra ## </font>
+                <div><font face="Cascadia Code" color="black" size="40">Paso 1 de 3</font></div>
+            </span>
+        </div>
+        <div className="mt-4">
+            <div className="row mt-100 w-100">
+                <div>
+                    <DireccionBilletera />
+                </div>
+            </div>
+            <Footer />
+        </div>
+    </div>
+}
+
+export default Historia29
