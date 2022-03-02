fd14a3fda47c35db7df8020a5fa554a5cb8a4637
Acissej9 <cotrina.ejci@gmail.com>
2022-03-01 20:56:18 -0500

    commit comprar y vender

--- components/BuyOrSellMoney.components.js
+++ components/BuyOrSellMoney.components.js
@@ -4,14 +4,14 @@ const BuyOrSellMoney = (props) =>{
 
     const [IUno, setIUno] = useState("")
     const [IUnoV, setIUnoV] = useState(0)
-
-    const [tc, settc] = useState("")
+ 
+    const [tc, settc] = useState(0)
+    const [IDos, setIDos] = useState("")
     const [IDosV, setIDosV] = useState(0)
     const [p, setp] = useState("")
     const [showS, setshowS] = useState("none")
     const [MostrarBgCompra, setMostrarBgCompra] = useState("#dedede")
     const [MostrarBgVenta, setMostrarBgVenta] = useState("#fff")
-
 
     const IDosVOnChange = (event) => {
         const IDosVOnChangeDT = event.target.value
@@ -19,13 +19,12 @@ const BuyOrSellMoney = (props) =>{
     }
 
     const tcOnChange = (event) => {
-        const tcOnChangeeDT = event.target.value
+        const tcOnChangeDT = event.target.value
         settc(tcOnChangeDT)
     }
-
     const IUnoVOnChange = (event) => {
         const IUnoVOnChangeDT = event.target.value
-        setIDosV(UnoVOnChangeDT)
+        setIUnoV(IUnoVOnChangeDT)
     }
 
     const EJectComprar = () => {
@@ -39,7 +38,6 @@ const BuyOrSellMoney = (props) =>{
         
       
     }
-
     const EJectVenta = () => {
         setshowS("inline-flex")
         setIUno("Bitcoins")
@@ -51,55 +49,3 @@ const BuyOrSellMoney = (props) =>{
         
       
     }
-
-    const EJectCalculo = () => {
-        console.log("funciona")
-        console.log(IUnoV)
-        console.log(tc)
-        var cal = IUnoV*tc
-        setIDosV.log(IDosV)
-        
-    }
-
-
-    return <aside>
-        <div className="card" >
-            <div className="card-body">
-                <h3>Comprar y vender Bitcoin</h3>
-                <div class="input-group mb-3">
-  <input type="number" class="form-control" placeholder="Tipo de cambio"   aria-label="Recipient's username" aria-describedby="basic-addon2"/>
-  
-</div>
-                <div class="container">
-            <div class="row" style={{"margin-bottom": "50px","margin-top": "100px"}}>
-                <div class="col-6 bold" style={{"font-weight": "bold","font-size":"26px","background":MostrarBgCompra,"cursor":"pointer","padding":"4px","border-radius":"10px","text-align":"center"}} onClick={ EJectComprar }>Comprar BTC</div>
-                <div class="col-6 bold" style={{"font-weight": "bold","font-size":"26px","background":MostrarBgVenta,"cursor":"pointer","padding":"4px","border-radius":"10px","text-align":"center"}} onClick={ EJectVenta }>Vender BTC</div>
-            </div>
-            <div class="row" style={{"width":"100%","margin": "0 auto", "display": showS}}>
-                <div class="col-6">
-                <label htmlFor="txt_password" className="form-label">Cantidad que {p}</label>
-                <div class="input-group mb-3">
-  <input type="number" class="form-control" placeholder= {IUno}  onChange={ IUnoVOnChange } aria-label="Recipient's username" aria-describedby="basic-addon2"/>
-  <div class="input-group-append">
-    <button class="btn btn-outline-secondary" onClick={ EJectCalculo } type="button">Calcular</button>
-  </div>
-</div>
-                        
-                </div>
-                <div class="col-6">
-                <label htmlFor="txt_password" className="form-label">Recibirás</label>
-                <div class="input-group mb-3">
-  <input type="number" class="form-control" placeholder= {IDos} onChange={ IDosVOnChange } defaultValue={ IDosV } aria-label="Recipient's username" aria-describedby="basic-addon2"/>
-  
-</div>
-                        
-                </div>
-            </div>
-        
-</div>
-               
-            </div>
-        </div>
-    </aside>
-}
-export default BuyOrSellMoney
