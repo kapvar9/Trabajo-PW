314f609ef2913a6cdd7110747ad6e210dfec9bae
kapvar9 <20183081@aloe.ulima.edu.pe>
2022-03-02 09:29:40 -0500

    correcion registro 2 componente

--- components/registro_paso2.components.js
+++ components/registro_paso2.components.js
@@ -1,4 +1,67 @@
+import { useState } from "react"
+
+const RegistroP2 = (props) => {
+    //Se crea variables de estado para el componente
+    const [txtCelularCliente, setTxtCelularCliente] = useState(0)
+    const [txtCorreoCliente, setTxtCorreoCliente] = useState("")
+    const [txtPasswordCliente, setTxtPasswordCliente] = useState("")
+
+    const txtCelularClienteChange =  (event) =>{
+        setTxtCelularCliente(event.target.value)
+    }  
+
+    const txtCorreoClienteChange =  (event) =>{
+        setTxtCorreoCliente(event.target.value)
+    }  
+
+    const txtPasswordClienteChange =  (event) =>{
+        setTxtPasswordCliente(event.target.value)
+    }
+
+    const butGuardarFinalClick = () => {
+
+        props.onGuardarCliente(txtCelularCliente, txtCorreoCliente,txtPasswordCliente)
+        location.href = "/esperaCliente"
+     }
+
+    return <div>
+        <h2 id="tituloRegistrar2">Registrar Paso 2</h2>
+        <div id="registrar2">
+        <div  className="container mt-8">
+
+        <form>
+            <div className="form-group">
+                <label htmlFor="exampleInputEmail1">Celular</label>
+                <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Ingresa el numero de celular" 
+                defaultValue={txtCelularCliente} onChange={txtCelularClienteChange}/>
+            </div>
+            <div className="form-group">
+                <label htmlFor="exampleInputEmail1">Correo</label>
+                <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"
+                defaultValue={txtCorreoCliente} onChange={txtCorreoClienteChange} />
+            </div>
+            <div className="form-group">
                 <label htmlFor="exampleInputPassword1">Password</label>
-<<<<<<< HEAD
+                <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Contraseña"
+                defaultValue={txtPasswordCliente} onChange={txtPasswordClienteChange}/>
+            </div>
+            <div className="form-group">
+                <label htmlFor="exampleInputPassword1">Repetir Password</label>
+                <input type="password" className="form-control" id="exampleInputPassword2" placeholder="Repetir contraseña" />
+            </div>
+            <div className="boton md-8">
+
+            <a href="../registrarClienteP1" className="btn btn-success">Regresar</a>
             &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-            <a onClick={ butGuardarClick } className="btn btn-success">Confirmar</a>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            {/*<a href="../esperaCliente" className="btn btn-success">Confirmar</a>*/}
+            <button className="btn btn-success" type="button" onClick={butGuardarFinalClick }>Siguiente</button>
+            </div>
+        </form>
+
+        </div>
+        </div>
+    </div>
+}
--- pages/registrarClienteP2.js
+++ pages/registrarClienteP2.js
@@ -6,45 +6,6 @@ import cliente from "../sequelize/models/cliente"
 
 function registrarClienteP2() {
 
-<<<<<<< HEAD
-    const guardarProyectoHandler = async (txtCelularCliente, txtCorreoCliente,txtPasswordCliente) =>{
-
-        //Obtener datos del local storage 
-        const clientesSTR = localStorage.getItem("clientes")
-        //Convertir poryectos con JSON PARSE
-        const clienteparcial = JSON.parse(clientesSTR)
-
-        //Actualizar datos de objeto
-        const cliente = {
-            nombre : clienteparcial.nombre,
-            apellidos : clienteparcial.apellidos,
-            dni: clienteparcial.dni,
-            celular:txtCelularCliente,
-            correo: txtCorreoCliente,
-            contraseña: txtPasswordCliente
-        }
-
-        const resp = await fetch("/api/clientes", {
-            method : "POST",
-            body : JSON.stringify(cliente)
-        })
-
-        const data = await resp.json()
-
-        //Se guardar cliente en local storage
-        /*localStorage.setItem("clientes",JSON.stringify(cliente))
-        location.href = "/registrarClienteP2"*/
-       
-
-    }
-
-=======
-    
-
-    const cliente = require('./registrarClienteP1');
-    const Nombre = cliente.nombre
-    const Apellidos = cliente.apellidos
-    const Dni = cliente.dni
 
     const guardarProyectoLS2 = (nombre,apellidos,dni,txtCelularCliente, txtCorreoCliente,txtPasswordCliente) =>{
         const cliente2 = {
@@ -66,17 +27,12 @@ function registrarClienteP2() {
     data.append()
 
 
->>>>>>> 21e0720bc564dae90e22afd54ace10e5a47ba119
     return <div>
         <header>
                 <Cabecera1/>  
             </header>
     
-<<<<<<< HEAD
-        <RegistroP2 onGuardarCliente = {guardarProyectoHandler}/>
-=======
         <RegistroP2 onGuardar ={guardarProyectoLS2}/>
->>>>>>> 21e0720bc564dae90e22afd54ace10e5a47ba119
         <Footer />
         </div>
   }
